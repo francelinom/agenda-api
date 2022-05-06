f8951a5f535a1f974970775767fcf2f59e968e7c
francelino <fmarcilio1989@gmail.com>
2022-05-05 22:46:22 -0300

    configuração aplicação para testar conexão com o banco

--- src/main/java/io/github/francelinom/agendaapi/AgendaApiApplication.java
+++ src/main/java/io/github/francelinom/agendaapi/AgendaApiApplication.java
@@ -1,11 +1,26 @@
 package io.github.francelinom.agendaapi;
 
+import io.github.francelinom.agendaapi.model.entity.Contato;
+import io.github.francelinom.agendaapi.model.repository.ContatoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AgendaApiApplication {
 
+	@Bean
+	public CommandLineRunner commandLineRunner(@Autowired ContatoRepository contatoRepository) {
+		return args -> {
+			Contato contato = new Contato();
+			contato.setNome("Francelino");
+			contato.setEmail("francelino@yahoo.com.br");
+			contato.setFavorito(false);
+			contatoRepository.save(contato);
+		};
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(AgendaApiApplication.class, args);
 	}
---
+++ src/main/java/io/github/francelinom/agendaapi/model/entity/Contato.java
@@ -0,0 +1,30 @@
+package io.github.francelinom.agendaapi.model.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter@Setter
+@NoArgsConstructor
+public class Contato {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String nome;
+
+    @Column
+    private String email;
+
+    @Column
+    private Boolean favorito;
+}
