67368d97d1c5e142ad6cfc77162afa87e7bd12e4
francelino <fmarcilio1989@gmail.com>
2022-05-11 08:30:05 -0300

    implementando o metodo de upload de fotos

--- src/main/java/io/github/francelinom/agendaapi/model/entity/Contato.java
+++ src/main/java/io/github/francelinom/agendaapi/model/entity/Contato.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 
 @Entity
 @Getter@Setter
@@ -27,4 +28,8 @@ public class Contato {
 
     @Column
     private Boolean favorito;
+
+    @Column
+    @Lob
+    private byte[] foto;
 }
